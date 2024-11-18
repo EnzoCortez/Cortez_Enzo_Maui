6b0b4f89f9557c41dffa8c71d700a961fa8d2b4c
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-11-17 19:13:17 -0500

    Implementacion de Grid Y Stack

--- MauiApp1/Grid.xaml.cs
+++ MauiApp1/Grid.xaml.cs
@@ -2,8 +2,49 @@ namespace MauiApp1;
 
 public partial class Grid : ContentPage
 {
-	public Grid()
-	{
-		InitializeComponent();
-	}
+    public Grid()
+    {
+        InitializeComponent();
+
+        var gridLayout = new Microsoft.Maui.Controls.Grid
+        {
+            RowDefinitions =
+            {
+                new RowDefinition { Height = GridLength.Star },
+                new RowDefinition { Height = GridLength.Star }
+            },
+            ColumnDefinitions =
+            {
+                new ColumnDefinition { Width = GridLength.Star },
+                new ColumnDefinition { Width = GridLength.Star }
+            }
+        };
+
+        var redBox = new BoxView { Color = Colors.Red };
+        Microsoft.Maui.Controls.Grid.SetRow(redBox, 0);
+        Microsoft.Maui.Controls.Grid.SetColumn(redBox, 0);
+
+        var blueBox = new BoxView { Color = Colors.Blue };
+        Microsoft.Maui.Controls.Grid.SetRow(blueBox, 0);
+        Microsoft.Maui.Controls.Grid.SetColumn(blueBox, 1);
+
+        var greenBox = new BoxView { Color = Colors.Green };
+        Microsoft.Maui.Controls.Grid.SetRow(greenBox, 1);
+        Microsoft.Maui.Controls.Grid.SetColumn(greenBox, 0);
+
+        var button = new Button
+        {
+            Text = "Ir a StackLayout",
+            Command = new Command(() => Navigation.PushAsync(new Stack()))
+        };
+        Microsoft.Maui.Controls.Grid.SetRow(button, 1);
+        Microsoft.Maui.Controls.Grid.SetColumn(button, 1);
+
+        gridLayout.Children.Add(redBox);
+        gridLayout.Children.Add(blueBox);
+        gridLayout.Children.Add(greenBox);
+        gridLayout.Children.Add(button);
+
+        Content = gridLayout;
+    }
 }
--- MauiApp1/Stack.xaml.cs
+++ MauiApp1/Stack.xaml.cs
@@ -2,8 +2,26 @@ namespace MauiApp1;
 
 public partial class Stack : ContentPage
 {
-	public Stack()
-	{
-		InitializeComponent();
-	}
+    public Stack()
+    {
+        InitializeComponent();
+
+        var stackLayout = new StackLayout
+        {
+            VerticalOptions = LayoutOptions.Center,
+            Children =
+            {
+                new Label { Text = "Nombre 1", FontSize = 24 },
+                new Label { Text = "Nombre 2", FontSize = 18 },
+                new Label { Text = "Nombre 3", FontSize = 14 },
+                new Button
+                {
+                    Text = "Ir a FlexLayout",
+                    Command = new Command(() => Navigation.PushAsync(new Flex()))
+                }
+            }
+        };
+
+        Content = stackLayout;
+    }
 }
